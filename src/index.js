bb746a45c0b497486dc4bf6bc552b875f91d0419
DianaTuei <memotuei@gmail.com>
2024-01-06 12:58:21 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener("submit",(e) => {
+    e.preventDefault();
+    handleToDo(e.target.newT.value)
+    form.reset()
+  })
 });
+
+function handleToDo (toDo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener("click", handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${toDo} `
+  li.appendChild (btn)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
